486c832162cee37396ab0412fdb433d9e38cebea
Rashid-C <anurashid105@gmail.com>
2026-02-24 01:13:52 +0400

    09-create product details  page

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,4 @@
 export const APP_NAME=process.env.NEXT_PUBLIC_APP_NAME || "AZC E-commerce"
 export const APP_SLOGAN=process.env.NEXT_PUBLIC_SLOGAN || "laptop"
 export const APP_DESCRIPTION=process.env.NEXT_PUBLIC_DESCRIPTION || "AZC_ELCTRONICTSLLC"
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
