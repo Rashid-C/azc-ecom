f5ed6ef3a21bf2f5ce66d8c5aea6b0502da17007
Rashid-C <anurashid105@gmail.com>
2026-02-23 21:14:10 +0400

    02-create web layout

---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME=process.env.NEXT_PUBLIC_APP_NAME || "AZC E-commerce"
+export const APP_SLOGAN=process.env.NEXT_PUBLIC_SLOGAN || "laptop"
+export const APP_DESCRIPTION=process.env.NEXT_PUBLIC_DESCRIPTION || "AZC_ELCTRONICTSLLC"
---
+++ lib/data.ts
@@ -0,0 +1,37 @@
+const data = {
+  headerMenus: [
+    {
+      name: "Today's Deals",
+      href: '/search?tag=todays-deal',
+    },
+    {
+      name: 'Best Sellers',
+      href: '/search?tag=best-seller',
+    },
+    {
+      name: 'Featured Products',
+      href: '/search?tag=featured',
+    },
+    {
+      name: 'New Arrivals',
+      href: '/search?tag=new-arrival',
+    },
+    {
+      name: 'Browing History',
+      href: '/#browsing-history',
+    },
+    {
+      name: 'Customer Service',
+      href: '/page/customer-service',
+    },
+    {
+      name: 'About Us',
+      href: '/page/about-us',
+    },
+    {
+      name: 'Help',
+      href: '/page/help',
+    },
+  ],
+}
+export default data
