78c15e7575fc2480abed016f8af34f8090a8c2f4
Rashid-C <anurashid105@gmail.com>
2026-02-25 16:13:12 +0400

    f

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -17,7 +17,7 @@ export default async function AdminLayout({
           <div className='flex h-16 items-center px-2'>
             <Link href='/' className=' '>
               <Image
-                src='/icons/logo.svg'
+                src='/icons/azclogo.svg'
                 width={48}
                 height={48}
                 alt={`${APP_NAME} logo`}
