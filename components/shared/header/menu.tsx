1c26b9e17b270a9eae6598dc1240c566aa7bd6e1
Rashid-C <anurashid105@gmail.com>
2026-02-25 16:12:14 +0400

    27-create admin dashboard

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,13 +1,35 @@
+import { EllipsisVertical } from 'lucide-react'
 import CartButton from './cart-button'
 import UserButton from './user-button'
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
 
-export default function Menu() {
+export default function Menu({ forAdmin = false }: { forAdmin?: boolean }) {
   return (
     <div className='flex justify-end'>
       <nav className='flex gap-3 w-full'>
         <UserButton />
         <CartButton />
+        {forAdmin ? null : <CartButton />}
 
+      </nav>
+
+      <nav className='md:hidden'>
+        <Sheet>
+          <SheetTrigger className='align-middle header-button'>
+            <EllipsisVertical className='h-6 w-6' />
+          </SheetTrigger>
+          <SheetContent className='bg-black text-white  flex flex-col items-start  '>
+            <SheetHeader className='w-full'>
+              <div className='flex items-center justify-between '>
+                <SheetTitle>Site Menu</SheetTitle>
+                <SheetDescription></SheetDescription>
+              </div>
+            </SheetHeader>
+
+            <UserButton />
+            <CartButton />
+          </SheetContent>
+        </Sheet>
       </nav>
     </div>
   )
