1295a711834024f7873764ed17e55c0c50ade604
Rashid-C <anurashid105@gmail.com>
2026-02-28 20:34:18 +0400

    m

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -1,7 +1,7 @@
 export default async function LoadingPage() {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen '>
-      <div className='p-6 rounded-lg shadow-md w-1/3 text-center text-red-700 bg-white'>
+      <div className='p-6 rounded-lg shadow-md w-1/3 text-center '>
         Loading...
         {/* Developer is working... */}
       </div>
