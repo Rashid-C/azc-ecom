1a3453912d448774056995e94040270cca24f739
Rashid-C <anurashid105@gmail.com>
2026-03-07 20:04:19 +0400

    Fixed Signup form function named wrong

--- app/[locale]/(auth)/sign-up/signup-form.tsx
+++ app/[locale]/(auth)/sign-up/signup-form.tsx
@@ -37,7 +37,7 @@ const signUpDefaultValues =
         confirmPassword: '',
       }
 
-export default function CredentialsSignInForm() {
+export default function SignUpForm() {
   const {
     setting: { site },
   } = useSettingStore()
